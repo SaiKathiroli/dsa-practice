0cf61280bdaac5628d0bf74209248fc34ccf28c9
Sai Kathiroli <89291521+SaiKathiroli@users.noreply.github.com>
2023-11-27 14:08:48 +0530

    Time: 247 ms (16.85%) | Memory: 55.3 MB (5.05%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,38 +1,32 @@
 class Solution {
     public String longestPalindrome(String s) 
     {
-     String max = "";
-     int len = Integer.MIN_VALUE;
-     for(int i=0;i<s.length();i++)
-     {
-         int l=i;
-         int r=i;
-         while(l>=0 && r<s.length() && s.charAt(l) == s.charAt(r))
-         {
-             if(r-l+1 > len)
-             {
-                len = r-l+1;
-                max = s.substring(l,r+1);
-             }
-             l-=1;
-             r+=1;
-         }
-        
-        l=i;
-        r=i+1;
-        while(l>=0 && r<s.length() && s.charAt(l) == s.charAt(r))
-        {
-            if(r-l+1 > len)
-            {
-                len = r-l+1;
-                max = s.substring(l,r+1);
-            }
-             l-=1;
-             r+=1;
+        String max = "";
+        int[][] arr = new int[s.length()][s.length()];
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            arr[i][i] = 1;
+            max = s.substring(i, i + 1);
         }
-     }   
 
-     return max;
+        // Check palindromes of length 2
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                arr[i][i + 1] = 1;
+                max = s.substring(i, i + 2);
+            }
+        }
 
+        // Check palindromes of length 3 and more
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j) && arr[i + 1][j - 1] == 1) {
+                    arr[i][j] = 1;
+                    max = s.substring(i, j + 1);
+                }
+            }
+        }
+        return max;
     }
 }
