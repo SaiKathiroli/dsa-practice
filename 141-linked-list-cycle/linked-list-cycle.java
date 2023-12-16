a538db0ca5071ce89ed1921e305590154af6a254
Sai Kathiroli <89291521+SaiKathiroli@users.noreply.github.com>
2023-12-16 13:19:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (36.93%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) 
+    {
+        ListNode temp = head;
+        while(temp!=null)
+        {
+            if(temp.val == Integer.MIN_VALUE)
+            {
+                return true;
+            }
+            temp.val = Integer.MIN_VALUE;
+            temp = temp.next;
+        }
+        return false;
+    }
+}
